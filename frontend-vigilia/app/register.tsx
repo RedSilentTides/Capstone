90c32774f1e16a0569cb773fc6120fe33dd5e069
Anaeli Neira <anaeli.neirav@gmail.com>
2025-10-24 23:39:34 -0300

    Actualizar registro

--- frontend-vigilia/app/register.tsx
+++ frontend-vigilia/app/register.tsx
@@ -72,7 +72,7 @@ export default function RegisterScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Crear Cuenta en VigilIAAAAAAAAAAAAAAAA</Text>
+      <Text style={styles.title}>Crear Cuenta en VigilIAAAAAA</Text>
 
       {/* Inputs para datos del usuario */}
       <TextInput
